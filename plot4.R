aa9315e2a2ee35169bbf4717f740bb12f3a95efd
xs2014 <xs2014@users.noreply.github.com>
2014-06-08 13:39:12 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -50,7 +50,7 @@ plot(feb$DateTime, feb$Sub_metering_1, pch = '',
 lines(feb$DateTime, feb$Sub_metering_1)
 lines(feb$DateTime, feb$Sub_metering_2, col = 'red')
 lines(feb$DateTime, feb$Sub_metering_3, col = 'blue')
-legend('topright', cex=0.9, lty=1, 
+legend('topright', cex=0.9, lty=1, bty='n',
        legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
        col = c('black', 'red', 'blue'))
 
